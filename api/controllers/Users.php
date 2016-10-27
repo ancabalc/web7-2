b7e4100a1e24e0f5285ea1226c55b795dbd8977b
Iacoblef Sergiu <iiacobov@yahoo.com>
2016-10-27 20:14:15 +0000

    update users

--- api/controllers/Users.php
+++ api/controllers/Users.php
@@ -2,18 +2,37 @@
 require "../api/models/UsersModels.php";
 class Users {
     public function updateUsers(){
-         if (isset($_POST["id"])) {
-            if (isset($_POST["name"]) || isset($_POST["description"]) || isset($_POST["image"])) {
+         if (isset($_POST["id"]) && isset($_POST["name"]) && isset($_POST["description"])) {
+             $_POST['image'] = '';
+             if(isset($_FILES["image"])){
+    $file=$_FILES["image"];
+    move_uploaded_file($file["tmp_name"],  "uploads/".$file["name"]);
+    $_POST['image'] = $file['name'];
+}
                 $usersModel = new UsersModel();
                 $row = $usersModel->updateUsers($_POST);
                 if ($row) {
                     $response = array("success"=>TRUE);  
                 }
                 else {
-                   $response = array("error"=>"error");        
+                   $response = array("error"=>"An error occured");        
                 }
                 return $response;
-                } 
-            } 
+                } else {
+                    $errors = array();
+                    if(!isset($_POST["id"])) {
+                        $erros ['id'] = 'Id is required';
+                    }
+                    if(!isset($_POST["name"])) {
+                        $erros['name'] = 'Name is required';
+                        
+                    }
+                    if(!isset($_POST["description"])) {
+                        $erros['description'] = 'Description is required';
+                        
+                    }
+                    $response = array("error"=>"An error occured",'messsage'=>$erros);   
+                    return $response;
+                }
     }
 }
--- api/models/UsersModels.php
+++ api/models/UsersModels.php
@@ -7,11 +7,11 @@ class UsersModel extends DB{
       function updateUsers($data) {
         $params = [':id' => $data["id"],
                     ':name' => $data["name"],
-                    ':description' => $data["description"]];
+                    ':description' => $data["description"],
+                    ':image' => $data['image']];
         
-        $sql = 'UPDATE users SET name=:name, description=:description  where id=:id';
+        $sql = 'UPDATE users SET name=:name, description=:description, image=:image where id=:id';
         $sth = $this->dbh->prepare($sql);
-        $sth->execute($params);
-        return $sth->rowCount(); 
+        return $sth->execute($params); 
     }
 }
