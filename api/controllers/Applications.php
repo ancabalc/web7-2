c2f9d81381c5e4fa260b082399e00f01c6282b4e
Hossu Raul <hossu.raul@yahoo.com>
2016-10-31 14:25:09 +0000

    Updated application

--- api/controllers/Applications.php
+++ api/controllers/Applications.php
@@ -1,10 +1,22 @@
 <?php
 
+require "api/models/ApplicationsModels.php";
+
 class Applications {
     function create(){
-        
+        $applicationsModels = new ApplicationsModels();
+        return $applicationsModels->createApplications();
     }
-    
+    function validateApplications() {
+        $appTitle = mysql_query("SELECT title FROM applications WHERE id = 1");
+        $result = mysql_fetch_array($appTitle);
+        
+        $appDescription = mysql_query("SELECT description FROM applications WHERE id = 1");
+        $result = mysql_fetch_array($appDescription);
+        
+        echo $result['appTitle'];
+        echo $result['appDescription'];
+    }
     function listApplications() {
         
       
