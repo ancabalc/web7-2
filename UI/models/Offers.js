1f80db8d32ab44fead92e78c8b356ba6bce815db
Jeanina Maria Ionescu <jeanina.ionescu@gmail.com>
2016-11-07 19:06:29 +0000

    list offers , add offer

---
+++ UI/models/Offers.js
@@ -0,0 +1,23 @@
+/*global $*/
+function Offers(){
+    this.models = [];
+}
+
+Offers.prototype.getOffers = function(){
+    var that = this;
+    return $.ajax({
+            url:"https://web7-2-jeanina.c9users.io/api/offers/create",
+            type:"POST",
+            dataType:"json",
+            success:function(resp){
+                for(var i = 0; i<resp.length; i++){
+                       var offer = new Offer(resp[i]);
+                       that.models.push(offer);
+                }
+            },
+            error:function(xhr,status,errorMessage){
+                console.log("Error status:"+status);
+            }
+    });
+}
+
